be6637846f416dc4645f050b3fed9e9d610542cc
artamonovoleg <artamonovav2003@gmail.com>
2021-12-23 23:39:15 +0300

    Write simple image

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,69 @@
 mod math;
-use math::Vector3;
+use math::{Vector3, unit_vector};
+
+#[derive(Copy, Clone)]
+pub struct Ray
+{
+    orig: Vector3,
+    dir: Vector3
+}
+
+impl Ray
+{
+    #[allow(dead_code)]
+    pub fn new(origin: Vector3, direction: Vector3) -> Self
+    {
+        Self { orig: origin, dir: direction }
+    }
+
+    #[allow(dead_code)]
+    pub fn origin(&self) -> Vector3
+    {
+        self.orig
+    }
+
+    #[allow(dead_code)]
+    pub fn direction(&self) -> Vector3
+    {
+        self.dir
+    }
+
+    #[allow(dead_code)]
+    pub fn at(&self, t : f64) -> Vector3
+    {
+        self.orig + t * self.dir
+    }
+}
+
+pub fn ray_color(r : Ray) -> Vector3
+{
+    let unit_direction: Vector3 = unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Vector3::new(1.0, 1.0, 1.0) + t * Vector3::new(0.5, 0.7, 1.0)
+}
 
 fn main() 
 {
-    let image_width = 256;
-    let image_height = 256;
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = ((image_width as f64) / aspect_ratio) as i32;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vector3::origin();
+    let horizontal = Vector3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vector3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = 
+        origin 
+        - horizontal / 2.0 
+        - vertical / 2.0 
+        - Vector3::new(0.0, 0.0, focal_length);
+
+    // Render
 
     println!("P3\n{} {} \n255\n", image_width, image_height);
     for j  in (0 .. (image_height - 1)).rev()
@@ -13,16 +72,24 @@ fn main()
 
         for i in 0 .. image_width
         {
-            let r : f64 = i as f64 / (image_width as f64 - 1.0);
-            let g : f64 = j as f64 / (image_height as f64 - 1.0);
-            let b : f64 = 0.25;
+            let u  = i as f64 / (image_width as f64 - 1.0);
+            let v = j as f64 / (image_height as f64 - 1.0);
+            let ray = Ray::new(
+                origin, 
+                lower_left_corner + u * horizontal + v * vertical - origin);
+            
+            let pixel_color = ray_color(ray);
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            println!("{} {} {}", ir, ig, ib);
+            write_color(pixel_color);
         }
     }
     eprint!("\nDone.\n");
+}
+
+pub fn write_color(v: Vector3)
+{
+    println!("{} {} {}", 
+        (255.999 * v.x()) as i32, 
+        (255.999 * v.y()) as i32, 
+        (255.999 * v.z()) as i32)
 }
--- src/math.rs
+++ src/math.rs
@@ -1,4 +1,4 @@
-use std::ops;
+use std::{ops, f64};
 
 #[derive(Copy, Clone)]
 pub struct Vector3
@@ -37,13 +37,26 @@ impl Vector3
     {
         self.e[2]
     }
+
+    #[allow(dead_code)]
+    pub fn length(&self) -> f64
+    {
+        self.length_squared().sqrt()
+    }
+
+    #[allow(dead_code)]
+    pub fn length_squared(&self) -> f64
+    {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+    }
 }
 
+/// Substract
 impl ops::Sub<Vector3> for Vector3 
 {
     type Output = Vector3;
 
-    fn sub(self, rhs: Vector3) -> Vector3
+    fn sub(self, rhs: Vector3) -> Self::Output
     {
         Vector3::new(self.e[0] - rhs.e[0], self.e[1] - rhs.e[1], self.e[2] - rhs.e[2])
     }
@@ -63,7 +76,7 @@ impl ops::Sub<f64> for Vector3
 {
     type Output = Vector3;
 
-    fn sub(self, rhs: f64) -> Vector3
+    fn sub(self, rhs: f64) -> Self::Output
     {
         Vector3::new(self.e[0] - rhs, self.e[1] - rhs, self.e[2] - rhs)
     }
@@ -79,11 +92,12 @@ impl ops::SubAssign<f64> for Vector3
     }
 }
 
+/// Add
 impl ops::Add<Vector3> for Vector3 
 {
     type Output = Vector3;
 
-    fn add(self, rhs: Vector3) -> Vector3
+    fn add(self, rhs: Vector3) -> Self::Output
     {
         Vector3::new(self.e[0] + rhs.e[0], self.e[1] + rhs.e[1], self.e[2] + rhs.e[2])
     }
@@ -103,7 +117,7 @@ impl ops::Add<f64> for Vector3
 {
     type Output = Vector3;
 
-    fn add(self, rhs: f64) -> Vector3
+    fn add(self, rhs: f64) -> Self::Output
     {
         Vector3::new(self.e[0] + rhs, self.e[1] + rhs, self.e[2] + rhs)
     }
@@ -119,8 +133,126 @@ impl ops::AddAssign<f64> for Vector3
     }
 }
 
-impl std::fmt::Display for Vector3 {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "[ {} {} {} ]", self.x(), self.y(), self.z())
+/// Multiply
+impl ops::Mul<Vector3> for Vector3
+{
+    type Output = Vector3;
+
+    fn mul(self, rhs: Vector3) -> Self::Output 
+    {
+        Vector3::new(self.e[0] * rhs.e[0], self.e[1] * rhs.e[1], self.e[2] * rhs.e[2])
+    }
+}
+
+impl ops::MulAssign<Vector3> for Vector3
+{
+    fn mul_assign(&mut self, rhs: Vector3) 
+    {
+        self.e[0] *= rhs.e[0];
+        self.e[1] *= rhs.e[1];    
+        self.e[2] *= rhs.e[2];    
+    }
+}
+
+impl ops::Mul<f64> for Vector3 
+{
+    type Output = Vector3;
+
+    fn mul(self, rhs: f64) -> Self::Output 
+    {
+        Vector3::new(self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs)
+    }
+}
+
+impl ops::Mul<Vector3> for f64
+{
+    type Output = Vector3;
+
+    fn mul(self, rhs: Vector3) -> Self::Output 
+    {
+        Vector3::new(self * rhs.e[0], self * rhs.e[1], self * rhs.e[2])
+    }
+}
+
+impl ops::MulAssign<f64> for Vector3
+{
+    fn mul_assign(&mut self, rhs: f64)
+    {
+        self.e[0] *= rhs;
+        self.e[1] *= rhs;    
+        self.e[2] *= rhs;
+    }
+}
+
+/// Divide
+impl ops::Div<Vector3> for Vector3 
+{
+    type Output = Vector3;
+    
+    fn div(self, rhs: Vector3) -> Self::Output 
+    {
+        Vector3::new(self.e[0] / rhs.e[0], self.e[1] / rhs.e[1], self.e[2] / rhs.e[2])    
+    }
+}
+
+impl ops::DivAssign<Vector3> for Vector3 
+{
+    fn div_assign(&mut self, rhs: Vector3) 
+    {
+        self.e[0] /= rhs.e[0];
+        self.e[1] /= rhs.e[1];    
+        self.e[2] /= rhs.e[2];    
+    }
+}
+
+impl ops::Div<f64> for Vector3
+{
+    type Output = Vector3;
+
+    fn div(self, rhs: f64) -> Self::Output 
+    {
+        Vector3::new(self.e[0] / rhs, self.e[1] / rhs, self.e[2] / rhs)    
+    }
+}
+
+impl ops::DivAssign<f64> for Vector3
+{
+    fn div_assign(&mut self, rhs: f64)
+    {
+        self.e[0] /= rhs;
+        self.e[1] /= rhs;
+        self.e[2] /= rhs;
+    }
+}
+
+#[allow(dead_code)]
+pub fn dot(u: Vector3, v: Vector3) -> f64
+{
+    u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]
+}
+
+#[allow(dead_code)]
+pub fn cross(u: Vector3, v: Vector3) -> Vector3
+{
+    Vector3::new
+    (
+        u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2], 
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]
+    )
+}
+
+#[allow(dead_code)]
+pub fn unit_vector(v: Vector3) -> Vector3
+{
+    v / v.length()
+}
+
+/// IO
+impl std::fmt::Display for Vector3 
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result 
+    {
+        write!(f, "[ {} {} {} ]", self.e[0], self.e[1], self.e[2])
     }
 }
